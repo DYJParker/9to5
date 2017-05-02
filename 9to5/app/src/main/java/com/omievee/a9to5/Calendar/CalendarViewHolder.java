351589ccbc1647fe12f19570a699a52d942c86c7
Dave Parker <DYJParker@gmail.com>
2017-05-02 12:39:30 -0400

    Clarified base implementation

--- 9to5/app/src/main/java/com/omievee/a9to5/Calendar/CalendarViewHolder.java
+++ 9to5/app/src/main/java/com/omievee/a9to5/Calendar/CalendarViewHolder.java
@@ -2,6 +2,7 @@ package com.omievee.a9to5.Calendar;
 
 import android.view.View;
 
+import com.omievee.a9to5.R;
 import com.omievee.a9to5.RecyclerView.AbstractBaseHolder;
 import com.omievee.a9to5.RecyclerView.AbstractBaseInformationObject;
 
@@ -10,10 +11,18 @@ import com.omievee.a9to5.RecyclerView.AbstractBaseInformationObject;
  */
 
 public class CalendarViewHolder extends AbstractBaseHolder {
+    //TODO add member data
 
     public CalendarViewHolder(View itemView) {
         super(itemView);
+        //VIEWTYPE content = LayoutInflater.from(itemView.getContext()).inflate(R.layout.YOUR_XML, null);
+        int padding = itemView.getResources().getDimensionPixelOffset(R.dimen.typ_margin);
+        int bottomPadding = padding + itemView.getResources().getDimensionPixelOffset(R.dimen.default_cardview_bottom_margin);
+        //content.setPadding(padding, padding, padding, bottomPadding);
 
+        //TODO assign stuff to your member variable views here
+
+        //((CardView) itemView).addView(content,0);
     }
 
     @Override
