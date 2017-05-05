bdcd0a6b6ccf1cbb3f59b3b98ddf3e8171488116
Justin Stanger <justinstanger@Justins-MacBook-Air.local>
2017-05-05 12:13:39 -0400

    Button works. Need error still

--- 9to5/app/src/main/java/com/omievee/a9to5/MainActivity.java
+++ 9to5/app/src/main/java/com/omievee/a9to5/MainActivity.java
@@ -4,12 +4,14 @@ import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,17 +19,23 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.omievee.a9to5.Calendar.CalendarCallbacks;
 import com.omievee.a9to5.JobScheduler.JobService;
 import com.omievee.a9to5.MTA_API.MTA_GetStatus;
+import com.omievee.a9to5.RecyclerView.AbstractBaseHolder;
 import com.omievee.a9to5.RecyclerView.AbstractBaseInformationObject;
 import com.omievee.a9to5.RecyclerView.InterfaceSingleton;
 import com.omievee.a9to5.RecyclerView.NetworkFailureImageHolder;
 import com.omievee.a9to5.RecyclerView.NetworkFailureObject;
 import com.omievee.a9to5.RecyclerView.RECYAdapter;
 import com.omievee.a9to5.Weather.WeatherCreate;
+import com.omievee.a9to5.Weather.WeatherInfoObject;
+import com.omievee.a9to5.Weather.WeatherViewHolder;
 
 import java.util.ArrayList;
 
@@ -50,8 +58,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         setSupportActionBar(toolbar);
 
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+    //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    //        setSupportActionBar(toolbar);
 
         //RecyclerView / LLM / Async Task
         DisplayMetrics metrics = getResources().getDisplayMetrics();
@@ -78,9 +86,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         }else {
             //things that need internet access here
             WeatherCreate.getCityWeather(sCityQuery, this, false);
+
             MTA_GetStatus.getMTAStatus(this);
         }
-        getSupportLoaderManager().initLoader(CALENDAR_LOADER, null, this);
+
 
         //JobInfo job = new JobInfo.Builder(JOB_ID,
         //        new ComponentName(this, JobService.class))
